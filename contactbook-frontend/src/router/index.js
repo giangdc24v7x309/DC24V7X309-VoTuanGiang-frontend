ce765e25a3b491d290f93c8edcf208728061a9f9
Vo Tuan Giang <giangdc24v7x309@dttx.ctu.edu.vn>
2025-11-17 00:56:33 +0700

    Update index.js

--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -8,17 +8,16 @@ const routes = [
     component: ContactBook,
   },
   {
-  path: "/:pathMatch(.*)*",
-  name: "notfound",
-  component: () => import("@/views/NotFound.vue"),
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
   },
   {
-        path: "/contacts/:id",
-        name: "contact.edit",
-        component: () => import("@/views/ContactEdit.vue"),
-        props: true // Truyền các biến trong $route.params vào làm props
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true // Truyền các biến trong $route.params vào làm props
   },
-  
 ];
 
 const router = createRouter({
