1736925084a41b6c00068113a94d992a3b070a5c
Vo Tuan Giang <giangdc24v7x309@dttx.ctu.edu.vn>
2025-11-16 23:33:30 +0700

    Update InputSearch.vue

--- contactbook-frontend/src/components/InputSearch.vue
+++ contactbook-frontend/src/components/InputSearch.vue
@@ -16,23 +16,23 @@ export default {
 </script>
 
 <template>
-  <div class="input-group">
-    <input
-      type="text"
-      class="form-control"
-      placeholder="Nhập thông tin cần tìm"
-      :value="modelValue"
-      @input="updateModelValue"
-      @keyup.enter="submit"
-    />
-    <div class="input-group-append">
-      <button
-        class="btn btn-outline-secondary"
-        type="button"
-        @click="submit"
-      >
-        <i class="fas fa-search"></i> Tìm kiếm
-      </button>
-    </div>
+<div class="input-group">
+  <input
+    type="text"
+    class="form-control"
+    placeholder="Nhập thông tin cần tìm"
+    :value="modelValue"
+    @input="updateModelValue"
+    @keyup.enter="submit"
+  />
+  <div class="input-group-append">
+    <button
+      class="btn btn-outline-secondary"
+      type="button"
+      @click="submit"
+    >
+      <i class="fas fa-search"></i> Tìm kiếm
+    </button>
   </div>
+</div>
 </template>
