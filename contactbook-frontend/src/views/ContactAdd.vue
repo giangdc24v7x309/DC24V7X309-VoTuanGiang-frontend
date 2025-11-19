1f0c3221f843340f57740231c5d7be0181130a4b
Vo Tuan Giang <giangdc24v7x309@dttx.ctu.edu.vn>
2025-11-19 16:54:47 +0700

    Cap nhat so thich và hon nhan

--- contactbook-frontend/src/views/ContactAdd.vue
+++ contactbook-frontend/src/views/ContactAdd.vue
@@ -19,6 +19,8 @@ export default {
                 address: "",
                 phone: "",
                 favorite: false,
+                hobbies: [],
+                maritalStatus: "",
             },
         };
     },
